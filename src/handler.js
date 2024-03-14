845d9ee9a8aa89f76c4b5e486d999442006a4f0b
Yossia Dwi Mahardika <yossiadwim@gmail.com>
2024-03-14 16:12:10 +0700

    fix bug

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,8 @@
 const { nanoid } = require("nanoid");
 const notes = require('./notes');
 
+
+
 const addNoteHandler = (request, h) => {
     const { title, tags, body } = request.payload;
     const id = nanoid(16);
--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const routes = require('./routes');
 
 const init = async () => {
     const server = Hapi.server({
-        port: 5000,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0' ,
+        port: 3000,
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
             cors: {
                 origin: ['*'],
